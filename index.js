49ad55ad2a9e1b0631a8ffa3df3739d182bd8500
MaxAnii <ansarbaba2000@gmail.com>
2024-04-18 13:46:22 +0530

    Config the cors.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const router = require("./routes/mainRoutes");
+const cors = require("cors");
 app.use(express.json());
+app.use(
+	cors({
+		origin: "*",
+		methods: ["GET", "POST"],
+		allowedHeaders: ["Content-Type"],
+	})
+);
 
 app.use("/data", router);
 
