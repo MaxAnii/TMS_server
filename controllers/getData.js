240c917f17c461e36d76962ffa2e12380eaf740f
MaxAnii <ansarbaba2000@gmail.com>
2024-05-02 18:05:17 +0530

    feat: add a function to get the quick data.

--- controllers/getData.js
+++ controllers/getData.js
@@ -1,23 +1,32 @@
 const pool = require("../config/db");
-const getData = async (req, res) => {
+const getQuickData = async (req, res) => {
 	try {
-		const { startingTimeStamp, endingTimeStamp } = req.params;
+		const { date } = req.params;
+		console.log(req.params);
 		const data = await pool.query(
-			`SELECT * FROM timeStampData WHERE devicetime BETWEEN $1 AND $2`,
-			[startingTimeStamp, endingTimeStamp]
+			`SELECT 
+			COALESCE(SUM(CASE WHEN user_type = '1' THEN 1 ELSE 0 END), 0) AS employees,
+			COALESCE(SUM(CASE WHEN user_type = '0' THEN 1 ELSE 0 END), 0) AS non_employees,
+			COUNT(DISTINCT device_id) AS total_devices
+		FROM 
+			tripInformation
+		WHERE on_board_time::date = $1`,
+			[date]
 		);
+
 		if (data.rows.length) {
-			res.json({
-				data: data.rows,
+			res.status(200).json({
+				totalEmployees: data.rows[0].employees,
+				totalNonEmployees: data.rows[0].non_employees,
+				totalDevice: data.rows[0].total_devices,
 			});
 		} else {
-			res.json({
-				message: "No data found",
-			});
+			res.status(404).send("No data found");
 		}
 	} catch (error) {
-		res.status(400).send("bad request");
+		console.log(error.message);
+		res.status(400).send(error.message);
 	}
 };
 
-module.exports = getData;
+module.exports = { getQuickData };
