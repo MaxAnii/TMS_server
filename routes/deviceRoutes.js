6cb77bf1418a28adb92f956ce03d803d3792b746
MaxAnii <ansarbaba2000@gmail.com>
2024-05-13 16:42:53 +0530

    feat: add a routes to add, update and get device details.

--- routes/deviceRoutes.js
+++ routes/deviceRoutes.js
@@ -2,12 +2,16 @@ const {
 	insertDeviceStatus,
 	updateDeviceStatus,
 	getStatus,
+	addDevice,
+	getDeviceList,
+	updateDeviceDetails,
 } = require("../controllers/deviceInfo");
 
 const deviceRouter = require("express").Router();
-
+deviceRouter.post("/add", addDevice);
+deviceRouter.get("/list", getDeviceList);
 deviceRouter.post("/set-status", insertDeviceStatus);
 deviceRouter.post("/update-status", updateDeviceStatus);
 deviceRouter.get("/get-status", getStatus);
-
+deviceRouter.put("/update-details", updateDeviceDetails);
 module.exports = deviceRouter;
