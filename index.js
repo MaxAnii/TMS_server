2fa32f71c46eef2905772a835b4579c63ca1e87a
MaxAnii <ansarbaba2000@gmail.com>
2024-05-13 16:40:24 +0530

    feat: add cors methods.

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const express = require("express");
 const app = express();
 const router = require("./routes/mainRoutes");
 const cors = require("cors");
+const getTimestamp = require("./libs/timeStamp");
 const port = process.env.PORT;
 app.use(express.json());
 app.use(
 	cors({
 		origin: "*",
-		methods: ["GET", "POST"],
+		methods: ["GET", "POST", "PUT", "DELETE"],
 		allowedHeaders: ["Content-Type"],
 	})
 );
 
 app.use("/", router);
-
+console.log(getTimestamp());
 app.get("/", (req, res) => {
 	res.send("i'm live");
 });
