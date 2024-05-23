6b0ce85687f5fee8a0e410b9944768d7871642d4
MaxAnii <ansarbaba2000@gmail.com>
2024-05-23 11:49:29 +0530

    refactor: change the name of the file.

--- libs/resetToken.js
+++ libs/resetToken.js
@@ -4,14 +4,13 @@ const getTimestamp = require("./timeStamp");
 const pool = require("../config/db");
 const { v4: uuid } = require("uuid");
 
-const DeletePerviousToken = async (email) => {
+const deletePerviousToken = async (email) => {
 	try {
-		console.log(email, "delete");
 		const data = await pool.query(
 			`SELECT * FROM resetToken WHERE "email" = $1`,
 			[email]
 		);
-		if (data.rows.length) {
+		if (data.rowCount) {
 			await pool.query(`DELETE FROM resetToken WHERE "email" = $1`, [email]);
 		}
 	} catch (error) {
@@ -22,16 +21,29 @@ const DeletePerviousToken = async (email) => {
 const generateToken = async (email) => {
 	try {
 		const token = uuid();
-		const timeStamp = getTimestamp();
-		await DeletePerviousToken(email);
+		const expires = new Date(new Date().getTime() + 3600 * 1000);
+		await deletePerviousToken(email);
 		const data = await pool.query(`INSERT INTO resetToken VALUES($1,$2,$3)`, [
 			token,
 			email,
-			timeStamp,
+			expires,
 		]);
 		await sendPasswordResetMail(email, token);
 	} catch (error) {
 		console.log(error.message);
 	}
 };
-module.exports = { generateToken };
+
+const getTokenInformation = async (token) => {
+	try {
+		const data = await pool.query(`SELECT * FROM resetToken WHERE id=$1`, [
+			token,
+		]);
+		if (data.rowCount) {
+			return data.rows[0];
+		}
+		return null;
+	} catch (error) {}
+};
+
+module.exports = { generateToken, getTokenInformation };
