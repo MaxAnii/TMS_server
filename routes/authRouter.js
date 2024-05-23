5c7aecf793b866a757b0085b1d8b5c02d523b721
MaxAnii <ansarbaba2000@gmail.com>
2024-05-23 11:50:54 +0530

    feat: adds route to verify token and to create new password.

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,14 @@
-const { login, sendPasswordResetLink } = require("../controllers/auth");
+const {
+	login,
+	sendPasswordResetLink,
+	verifyResetToken,
+	createNewPassword,
+} = require("../controllers/auth");
 
 const authRouter = require("express").Router();
 
 authRouter.post("/login", login);
 authRouter.get("/send-reset-link/:email", sendPasswordResetLink);
-
+authRouter.get("/verfiy-reset-token/:token", verifyResetToken);
+authRouter.post("/newpassword", createNewPassword);
 module.exports = authRouter;
