56ed185a129fed1d1b0a79518af6cb83a82ad28a
MaxAnii <ansarbaba2000@gmail.com>
2024-05-24 12:39:57 +0530

    feat: add routes to set up the cookies.

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,11 +3,15 @@ const {
 	sendPasswordResetLink,
 	verifyResetToken,
 	createNewPassword,
+	getCookie,
+	logout,
 } = require("../controllers/auth");
 
 const authRouter = require("express").Router();
 
 authRouter.post("/login", login);
+authRouter.get("/logout", logout);
+authRouter.get("/check-cookie", getCookie);
 authRouter.get("/send-reset-link/:email", sendPasswordResetLink);
 authRouter.get("/verfiy-reset-token/:token", verifyResetToken);
 authRouter.post("/newpassword", createNewPassword);
